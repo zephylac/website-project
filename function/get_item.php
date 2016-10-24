2e312bfd9a9ef8a1b41fe94c1eae1ab783873ab3
zephylac <thibo.brocherieux@gmail.com>
2016-10-24 11:45:31 +0200

    Update get_item.php

--- function/get_item.php
+++ function/get_item.php
@@ -1,33 +1,32 @@
 <?php
 	if(!isset($_POST["filtre"])){
-			$order = 'TypeJeux';	
+		$order = 'TypeJeux';	
 	}
 	else{
-			$order = $_POST["order"];
-      $result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` ORDER BY ".$order."");
-			while ($i < mysql_num_fields($result)){
-          $fieldName = mysql_field_name($result, $i);
-          //Here we can add filter if we don't want to show some column.
-          echo '<th>' . $fieldName . '</th>';
-          $i = $i + 1;
-        }
+		$order = $_POST["order"];
+      		$result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` ORDER BY ".$order."");
+	 	while ($i < mysql_num_fields($result)){
+          		$fieldName = mysql_field_name($result, $i);
+          		//Here we can add filter if we don't want to show some column.
+          		echo '<th>' . $fieldName . '</th>';
+          		$i = $i + 1;
+        	}	
         echo '</tr>';
         $i = 0;
 
         while ($row = mysql_fetch_row($result)){
-          echo '<tr>';
-          $count = count($row);
-          $y = 0;
-          while ($y < $count)
-          {
-                  $c_row = current($row);
-                  //Here we can add filter if we don't want to show some column
-                  echo '<td>' . $c_row . '</td>';
-                  next($row);
-                  $y = $y + 1;
-          }
-          echo '</tr>';
-          $i = $i + 1;
+         	echo '<tr>';
+          	$count = count($row);
+          	$y = 0;
+          	while ($y < $count){
+                  	$c_row = current($row);
+                  	//Here we can add filter if we don't want to show some column
+                  	echo '<td>' . $c_row . '</td>';
+                  	next($row);
+                  	$y = $y + 1;
+          	}
+          	echo '</tr>';
+          	$i = $i + 1;
         }
         mysql_free_result($result);
     	?>
