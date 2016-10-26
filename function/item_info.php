c24272d2e4154900d61df69e7f4ecc2484c24e20
zephylac <thibo.brocherieux@gmail.com>
2016-10-26 13:42:31 +0200

    Update item_info.php

--- function/item_info.php
+++ function/item_info.php
@@ -6,7 +6,10 @@
 
           while ($donnees = mysql_fetch_array($Reponse) ){
           //Display the result
-          echo "<p> ".$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4]."</p>";
-          
+          echo "<p> ".$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4]."</p>
+		<form id='item_res' method='post' action=''>
+		<input type='hidden' name='jeux_res' value=".$donnees[0].">
+		</form>
+		<a href='reserver.php' onclick='document.getElementById('item_res').submit()'>Reserver</a>";
           }
 ?>
