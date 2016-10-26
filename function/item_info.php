1124a56e4da369c25519030fd59269bfe156fa25
zephylac <thibo.brocherieux@gmail.com>
2016-10-26 13:17:35 +0200

    Create item_info.php
    Display the info fr

---
+++ function/item_info.php
@@ -0,0 +1,12 @@
+//Display the info from the item selected in the game list
+<?php
+  if(isset($_POST["filter"])){
+			$jeux = $_POST["jeux"];
+      		$result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` WHERE `Nom`=".$jeux."");
+
+          while ($donnees = mysql_fetch_array($Reponse) ){
+          //Display the result
+          echo "<p> ".$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4]."</p>";
+          
+          }
+?>
