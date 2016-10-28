45f3a9a6fa427f98816abdcef3bc160761fd66d2
zephylac <thibo.brocherieux@gmail.com>
2016-10-28 11:29:34 +0200

    Update get_item.php

--- function/get_item.php
+++ function/get_item.php
@@ -1,11 +1,16 @@
 <?php
+	require 'db_controller.php';
+
 	if(!isset($_POST["filter"])){
 		//If there is no filter given, it will be sorted by DEFAULT whith the following value
-		$order = 'TypeJeux';	
+		$order = 'Nom';	
 	}
 	else{
 		$order = $_POST["order"];
-      		$result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` ORDER BY ".$order."");
+	}
+		$sql = 'SELECT * FROM jeux NATURAL JOIN jeuxludotheque ORDER BY '.$order.'';
+		$result = db_request($sql);
+		//$result = db_request("SELECT * FROM `jeux` NATURAL JOIN `jeuxludotheque` ORDER BY ".$order."");
 	 	while ($i < mysql_num_fields($result)){
           		$fieldName = mysql_field_name($result, $i);
           		//Here we can add filter if we don't want to show some column.
@@ -16,17 +21,18 @@
         $i = 0;
 
         while ($row = mysql_fetch_row($result)){
-         	echo '<tr>
-		<form id='.$c_row.' action="../info.php">
-		<input type="hidden" name="jeux" value="'.$c_row.'"/>
-		</form>';
 		
           	$count = count($row);
           	$y = 0;
           	while ($y < $count){
                   	$c_row = current($row);
                   	//Here we can add filter if we don't want to show some column
-                  	echo '<td><a href='#' onclick="document.getElementById('.$c_row.').submit()">' . $c_row . '</a></td>';
+			echo '<tr><form id='.$c_row.' action="../info.php">
+				<input type="hidden" name="jeux" value="'.$c_row.'"/>
+				</form>';
+
+			
+			echo '<td><a href="#" onclick="document.getElementById('.$c_row.').submit()">' . $c_row . '</a></td>';
                   	next($row);
                   	$y = $y + 1;
           	}
@@ -34,4 +40,4 @@
           	$i = $i + 1;
         }
         mysql_free_result($result);
-    	?>
+?>
