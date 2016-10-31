ec5d2390d7da04bda4790d2ee2330b3c3646b071
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 16:46:22 +0100

    Create info.php
    moved from repositery towards subfolder view

---
+++ view/info.php
@@ -0,0 +1,21 @@
+//This file show the user all the game contained in the database
+<?php require('function/db_controller.php');?>
+<!DOCTYPE hmtl>
+<html>
+        <head>
+                <meta charset="UTF8">
+		<link href="main.css" rel="stylesheet" type="text/css"></link>  
+		<title> Ludothèque de SPI-ville </title>
+        </head>
+        <body>
+	<?php  include './view/header.php';?>
+  <div id="nav-mid">
+	  <?php  include './function/item_info.php';?> 
+	</div>
+</div>
+		
+		<div id="footer">
+			<p><center>Mentions légales|Charte de confidentialité|Plan du site</p>
+		</div>
+        </body>
+</html>
