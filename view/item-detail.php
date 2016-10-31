53f7ebd1f2b5920a59cafe7df95e894b6435b9b4
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 18:11:39 +0100

    Update item-detail.php

--- view/item-detail.php
+++ view/item-detail.php
@@ -6,9 +6,11 @@
     $result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` WHERE `Nom`=".$jeux."");
     while ($donnees = mysql_fetch_array($Reponse) ){
     	//Display the result
-      echo '<p> '.$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4].'</p>';
-			echo '<form id="item_res" method="post" action='A DETERMINER'>';
-			echo '<input type="submit" name="jeux_res" value=".$jeux."></form>';
-			echo '<a href="../reserver-jeux.php" onclick="document.getElementById("item_res").submit()">Reserver</a>';
+		echo '<div id="nav-mid">';
+	    	echo '<p> '.$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4].'</p>';
+	    	echo '<form id="item_res" method="post" action='A DETERMINER'>';
+		echo '<input type="submit" name="jeux_res" value=".$jeux."></form>';
+		echo '<a href="../reserver-jeux.php" onclick="document.getElementById("item_res").submit()">Reserver</a>';
+	    	echo '</div>';
           }
 ?>
