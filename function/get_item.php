e9adbdac3d23e42cba7a75874efac135c58a5a9f
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 16:58:07 +0100

    Update get_item.php
    Fixed some issues

--- function/get_item.php
+++ function/get_item.php
@@ -11,33 +11,29 @@
 		$sql = 'SELECT * FROM jeux NATURAL JOIN jeuxludotheque ORDER BY '.$order.'';
 		$result = db_request($sql);
 		//$result = db_request("SELECT * FROM `jeux` NATURAL JOIN `jeuxludotheque` ORDER BY ".$order."");
-	 	while ($i < mysql_num_fields($result)){
-          		$fieldName = mysql_field_name($result, $i);
-          		//Here we can add filter if we don't want to show some column.
-          		echo '<th>' . $fieldName . '</th>';
-          		$i = $i + 1;
-        	}	
-        echo '</tr>';
-        $i = 0;
-
-        while ($row = mysql_fetch_row($result)){
-		
-          	$count = count($row);
-          	$y = 0;
-          	while ($y < $count){
-                  	$c_row = current($row);
-                  	//Here we can add filter if we don't want to show some column
-			echo '<tr><form id='.$c_row.' action="../info.php">
-				<input type="hidden" name="jeux" value="'.$c_row.'"/>
-				</form>';
-
-			
-			echo '<td><a href="#" onclick="document.getElementById('.$c_row.').submit()">' . $c_row . '</a></td>';
-                  	next($row);
-                  	$y = $y + 1;
-          	}
-          	echo '</tr>';
-          	$i = $i + 1;
+	 	echo'<table id="table-jeux"><tr>';
+		while ($i < mysql_num_fields($result)){
+    	$fieldName = mysql_field_name($result, $i);
+      //Here we can add filter if we don't want to show some column.
+      echo '<th>' . $fieldName . '</th>';
+      $i = $i + 1;
+    }	
+    echo '</tr>';
+    $i = 0;
+			while ($row = mysql_fetch_row($result)){		
+      	$count = count($row);
+        $y = 0;
+        while ($y < $count){
+        	$c_row = current($row);
+          //Here we can add filter if we don't want to show some column
+					echo '<tr><form id='.$c_row.' action="../info.php"><input type="hidden" name="jeux" value="'.$c_row.'"/></form>';
+					echo '<td><a href="#" onclick="document.getElementById('.$c_row.').submit()">' . $c_row . '</a></td>';
+          next($row);
+          $y = $y + 1;
         }
-        mysql_free_result($result);
+        echo '</tr>';
+        $i = $i + 1;
+      }
+      mysql_free_result($result);
+			echo '</table>';
 ?>
