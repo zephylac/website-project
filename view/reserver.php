9cbb3622b9e1dba40065343fe4ec3dc37f7c17e4
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 16:47:56 +0100

    Create reserver.php

---
+++ view/reserver.php
@@ -0,0 +1,20 @@
+<?php require('../function/db_controller.php');?>
+<!DOCTYPE hmtl>
+<html>
+        <head>
+                <meta charset="UTF8">
+		<link href="main.css" rel="stylesheet" type="text/css"></link>  
+		<title> Ludothèque de SPI-ville </title>
+        </head>
+        <body>
+	<?php  include 'header.php';?>
+  <div id="nav-mid">
+	  <?php  include '../function/new_withdraw.php';?>
+	</div>
+</div>
+		
+		<div id="footer">
+			<p><center>Mentions légales|Charte de confidentialité|Plan du site</p>
+		</div>
+        </body>
+</html>
