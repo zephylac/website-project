455dbae2afd1c30961c1331ae69b43c34e710e01
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 16:35:28 +0100

    Create connexion.php

---
+++ view/connexion.php
@@ -0,0 +1,21 @@
+<div id="nav-mid-connexion">
+  <form id="formulaire" method="post" action="./function/login.php">
+	  <h2 id="identification" >S'identifier</h2>
+		<table>
+			<tr>
+				<td class="form"> Numéro carte d'adhérent : </td>
+			</tr>
+			<tr>
+				<td> <input class="case" type="number"/><br /> </td>
+			</tr>
+				<td class="form"> Mot de passe : </td>
+			</tr>
+			<tr>	
+				<td> <input class="case" type="password"/><br /></td>
+			</tr>
+		</table>
+		<input class="bouton" type="submit" value="Connexion"/><br />
+		<br /><p> Vous êtes nouveau à la Ludothèque? </p>
+		<a href="inscription.php"> Inscription </a>
+	</form>
+</div>
