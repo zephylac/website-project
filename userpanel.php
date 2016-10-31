7cc05abde3be6d7ed769659eac044b1aed13cfb5
zephylac <thibo.brocherieux@gmail.com>
2016-10-31 16:27:33 +0100

    Create userpanel.php

---
+++ userpanel.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE hmtl>
+<html>
+        <head>
+                <meta charset="UTF8">
+		<link href="main.css" rel="stylesheet" type="text/css"></link>  
+		<title> Ludothèque de SPI-ville </title>
+        </head>
+        <body>
+	<?php  include './view/header.php';?>
+  <div id="nav-mid">
+			<?php  include './view/reservation.php';?>
+	</div>
+</div>
+		
+		<div id="footer">
+			<p><center>Mentions légales|Charte de confidentialité|Plan du site</p>
+		</div>
+        </body>
+</html>
