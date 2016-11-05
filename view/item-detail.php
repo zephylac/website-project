aa55857445dfdc26e8e5d3c1f12cec4c2407ebd2
zephylac <thibo.brocherieux@gmail.com>
2016-11-05 12:33:21 +0100

    Update item-detail.php

--- view/item-detail.php
+++ view/item-detail.php
@@ -1,16 +1,18 @@
-//Display the info from the item selected in the game list
-<?php
-	require ('../function/db_controller.php');
-  if(isset($_POST["jeux"])){
-		$jeux = $_POST["jeux"];
-    $result = db_request("SELECT * FROM `Jeux` NATURAL JOIN `JeuxLudotheque` WHERE `Nom`=".$jeux."");
-    while ($donnees = mysql_fetch_array($Reponse) ){
-    	//Display the result
-		echo '<div id="nav-mid">';
+<?php  
+require ('./function/db_controller.php');
+  if(isset($_GET["jeux"])){
+	  $jeux = $_GET["jeux"];
+	  echo $jeux;
+    	$result = db_request("SELECT * FROM FC_grp1_Jeux NATURAL JOIN FC_grp1_JeuxLudotheque WHERE Nom='".$jeux."'");
+	echo '<div id="nav-mid">';
+	while ($donnees = mysql_fetch_array($result) ){
+    		//Display the result
 	    	echo '<p> '.$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4].'</p>';
-	    	echo '<form id="item_res" method="post" action='A DETERMINER'>';
-		echo '<input type="submit" name="jeux_res" value=".$jeux."></form>';
-		echo '<a href="../reserver-jeux.php" onclick="document.getElementById("item_res").submit()">Reserver</a>';
-	    	echo '</div>';
-          }
+	}
+	echo '</div>';
+  }
+  else{
+	  echo '<p>PB</p>';
+  }
 ?>
+
