22443e129959db5f78af9784b83a3d120ec4e18e
zephylac <thibo.brocherieux@gmail.com>
2016-11-05 14:41:28 +0100

    Update item-detail.php

--- view/item-detail.php
+++ view/item-detail.php
@@ -1,18 +1,17 @@
 <?php  
 require ('./function/db_controller.php');
   if(isset($_GET["jeux"])){
-	  $jeux = $_GET["jeux"];
-	  echo $jeux;
+	$jeux = $_GET["jeux"];
     	$result = db_request("SELECT * FROM FC_grp1_Jeux NATURAL JOIN FC_grp1_JeuxLudotheque WHERE Nom='".$jeux."'");
 	echo '<div id="nav-mid">';
 	while ($donnees = mysql_fetch_array($result) ){
     		//Display the result
 	    	echo '<p> '.$donnees[0].$donnees[1].$donnees[2].$donnees[3].$donnees[4].'</p>';
 	}
+	echo '<a href="./reserver-jeux.php?jeux='.$donnees[0].'">Reserver</a>';
 	echo '</div>';
   }
   else{
 	  echo '<p>PB</p>';
   }
 ?>
-
