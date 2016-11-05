046c1059fbca094513ff402f6b34abb32854ce4c
zephylac <thibo.brocherieux@gmail.com>
2016-11-05 14:40:50 +0100

    Update item-reservation.php

--- view/item-reservation.php
+++ view/item-reservation.php
@@ -1,22 +1,21 @@
 <?php
-    require('../function/db_controller.php');
-    require('../function/connect.php');
+    require('./function/db_controller.php');
     $verif = verif();
     if($verif != 1){
         //On renvoie l'utilisateur à la page de connexion
         header("location: connexion.php");
     }
     else{
-      if(isset($_POST["jeux_res"])){
+      if(isset($_GET["jeux"])){
         
         //Var relative to the item withdraw
-        $item_withdraw = $_POST['item'];
-        $date = $_POST['date'];
+        $item_withdraw = $_GET['jeux'];
+        //$date = $_POST['date'];
         //Var relative to the user
         $nom = $_COOKIE['nom'];
         $id = $_COOKIE['id'];
           // Verifiy if the client basket is not full
-          $sql='SELECT * FROM Paniers WHERE id='.$id.'';
+          $sql="SELECT * FROM FC_grp1_Paniers WHERE Client='".$nom."'";
           $result = db_request($sql);
           if(mysql_num_rows($result) >= 3){
             //Alert the user that he reach the limit of reservation
@@ -26,24 +25,30 @@
           }
           else{
             // Verifiy if the game is still available
-            $sql='SELECT NbJeuxDispos FROM Jeux_Ludotheque WHERE Nom='.$item_withdraw.'';
+            $sql="SELECT NbJeuxDispos FROM FC_grp1_JeuxLudotheque WHERE Nom='".$item_withdraw."'";
             $result = db_request($sql);
-            $number = mysql_fetch_array($reponse)
-            if($number[0] == 0){
-              //Alert the user that the games chosen is no longer available
-              echo '<script language="javascript">';
-              echo 'alert("Le jeu n\'est plus disponible")';
-              echo '</script>';
-            }
-            else{
-              // Insertion d'un jeux dans le panier du client
-              $sql = 'INSERT INTO Paniers (id, nom, creneau) VALUES ('.$id.', '.$item_withdraw.', '.$date.')';
-              $result = db_request($sql);
-              if($result){
-                $sql = 'UPDATE Jeux_Ludotheque SET NbJeuxDispos = NbJeuxDispos - 1 WHERE Nom='.$item_withdraw.'';
-                $result = db_request($sql);
-              }
-            }
+	    while ($donnees = mysql_fetch_array($result)){ 
+		$value = $donnees[0];    
+	    }   
+	        echo $value;
+	    	if($value == 0){
+              	//Alert the user that the games chosen is no longer available
+              	echo '<script language="javascript">';
+              	echo 'alert("Le '.$item_withdraw.' n\'est plus disponible")';
+              	echo '</script>';
+           	}
+		else{
+			$date = '14h';
+			// Insertion d'un jeux dans le panier du client
+		    	$sql = "INSERT INTO FC_grp1_Paniers (Jeux, Client, Creneau) VALUES ('".$item_withdraw."', '".$nom."', '".$date."')";
+              		$result = db_request($sql);
+              		if($result){
+                		$sql = "UPDATE FC_grp1_JeuxLudotheque SET NbJeuxDispos = NbJeuxDispos - 1 WHERE Nom='".$item_withdraw."'";
+                	$result = db_request($sql);
+              		}
+            	}
+            //
           }
+      }
     }
 ?>
