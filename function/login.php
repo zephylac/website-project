37b6b2654d3b3e72f79c3630dfb95e31bccb917d
zephylac <thibo.brocherieux@gmail.com>
2016-11-07 23:40:58 +0100

    Update login.php

--- function/login.php
+++ function/login.php
@@ -7,20 +7,19 @@ if(isset($_POST["connect"])){
 		$id = $_POST["id"];
 		$sql = 'SELECT Client FROM FC_grp1_Membres WHERE Client="'.$nom.'" AND Id="'.$id.'"';
 		$result = db_request($sql);
-		while ($donnees = mysql_fetch_array($result) ){
-			if($donnees[0] != NULL){
-				//Traitement si la personne a réussi à se connecter
-				//On set des cookies d'une durée de 7 jours ainsi l'utilisateur n'a pas besoin de se connecter tous le temps
-				setcookie('nom', $nom, time() + 7*24*3600, '/', null, false, true);
-				setcookie('id', $id, time() + 7*24*3600, '/', null, false, true);
-				//On redirige l'utilisateur vers son dashboard
-				header("location: ../userpanel.php");
-				
-			}
-			else{
-				echo " Votre login ou votre mot de passe est faux";
-			}
+		if(mysql_num_rows($result) == 0){
+			//Traitement si la personne a rentré un id/nom faux
+			header("location: ../view/connexion.php");	
 		}
+		else{
+			//Traitement si la personne a réussi à se connecter
+			//On set des cookies d'une durée de 7 jours ainsi l'utilisateur n'a pas besoin de se connecter tous le temps
+			setcookie('nom', $nom, time() + 7*24*3600, '/', null, false, true);
+			setcookie('id', $id, time() + 7*24*3600, '/', null, false, true);
+			//On redirige l'utilisateur vers son dashboard
+			header("location: ../userpanel.php");		
+		}
+		
 	}
 }	
 ?>
