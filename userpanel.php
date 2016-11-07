3d9413936406e20e52b1361259abb37631804490
zephylac <thibo.brocherieux@gmail.com>
2016-11-07 22:04:07 +0100

    Update userpanel.php

--- userpanel.php
+++ userpanel.php
@@ -6,13 +6,12 @@
 		<title> Ludothèque de SPI-ville </title>
         </head>
         <body>
-	<?php  include './view/header.php';?>
-  	<?php  include './view/dashboard.php';?>
-	<!--le div suivant appartient au main-bloc contenu dans header.php-->
-	</div> 
-		
-	<div id="footer">
-		<p><center>Mentions légales|Charte de confidentialité|Plan du site</p>
-	</div>
+			<?php  include './view/header.php';?>
+  			<?php  include './view/dashboard.php';?>
+			<!--le div suivant appartient au main-bloc contenu dans header.php-->
+		</div> 	
+		<div id="footer">
+			<p><center>Mentions légales|Charte de confidentialité|Plan du site</p>
+		</div>
         </body>
 </html>
