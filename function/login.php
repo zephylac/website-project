2421f599863f43d6ff04ec2254fa1efeb0733baf
zephylac <thibo.brocherieux@gmail.com>
2016-11-07 09:12:29 +0100

    Update login.php

--- function/login.php
+++ function/login.php
@@ -5,14 +5,14 @@ if(isset($_POST["connect"])){
 	if(!empty($_POST["nom"])&&!empty($_POST["id"])){
 		$nom = $_POST["nom"];
 		$id = $_POST["id"];
-    $sql = 'SELECT Nom FROM ... WHERE Nom='.$nom.' AND id='.$id'';
+    $sql = 'SELECT Client FROM FC_grp1_Paniers WHERE Client="'.$nom.'" AND Id="'.$id'"';
     $result = db_request($sql);
     while ($donnees = mysql_fetch_array($result) ){
 		if($donnees[0] != NULL){
 			//Traitement si la personne a réussi à se connecter
 			//On set des cookies d'une durée de 7 jours ainsi l'utilisateur n'a pas besoin de se connecter tous le temps
-			setcookie('nom', $nom, time() + 7*24*3600, null, null, false, true);
-			setcookie('id', $id, time() + 7*24*3600, null, null, false, true);
+			setcookie('nom', $nom, time() + 7*24*3600, "/", null, false, true);
+			setcookie('id', $id, time() + 7*24*3600, "/", null, false, true);
 			//On redirige l'utilisateur vers son dashboard
 			header("location: ../view/dashboard.php");
 		}
